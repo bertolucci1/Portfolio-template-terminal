6287ff7797f71e7eeaf756397b14cfa623560f57
Elías Costilla <94435079+bertolucci1@users.noreply.github.com>
2023-01-02 05:02:51 +0000

    Updates

---
+++ precache-manifest.d6f90f9b9e3b3b0ec8c9e831798d5076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cfbf2f6c32570b3f82",
+    "url": "/react-terminal-portfolio/static/css/main.4c6c0c33.chunk.css"
+  },
+  {
+    "revision": "09cfbf2f6c32570b3f82",
+    "url": "/react-terminal-portfolio/static/js/main.09cfbf2f.chunk.js"
+  },
+  {
+    "revision": "d618831c21a9091e7ded",
+    "url": "/react-terminal-portfolio/static/js/1.d618831c.chunk.js"
+  },
+  {
+    "revision": "4b4e50c2ee7f52aeb153",
+    "url": "/react-terminal-portfolio/static/js/runtime~main.4b4e50c2.js"
+  },
+  {
+    "revision": "d2966867924bd6479928d1b234580439",
+    "url": "/react-terminal-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-terminal-portfolio/precache-manifest.4fda5c3fc86818938c5c093db254a719.js"
+  "/react-terminal-portfolio/precache-manifest.d6f90f9b9e3b3b0ec8c9e831798d5076.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-terminal-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute("/react-terminal-portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
